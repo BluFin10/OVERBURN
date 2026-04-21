be2f51264d38559b05d93c75d438876a5e676a9b
BluFin10 <Finntheobald74@gmail.com>
2026-04-21 14:30:25 -0600

    Finished dash charge Ui, did score UI and started minimap UI

--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -1,4 +1,6 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class HUDManager : MonoBehaviour
@@ -10,16 +12,22 @@ public class HUDManager : MonoBehaviour
     private PlayerController _player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private Image fillImage;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI scoreTextShadow;
 
     void Start()
     {
+        _gameManager = FindAnyObjectByType<GameManager>();
         _player = FindFirstObjectByType<PlayerController>();
+        fillImage = GameObject.Find("CapsuleEmpty").GetComponent<Image>();
     }
 
     void Update()
     {
         float t = (_player.dashPower - _player.minDashPower) / (_player.maxDashPower - _player.minDashPower);
-        fillImage.fillAmount = t;
+        fillImage.fillAmount = 1-t;
+        scoreText.text = _gameManager.heatedScore.ToString();
+        scoreTextShadow.text = _gameManager.heatedScore.ToString();
     }
 }
 
